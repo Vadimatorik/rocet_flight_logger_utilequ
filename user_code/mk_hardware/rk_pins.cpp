15b849109730d12c87ec0dc88730f66716357193
Vadim Deryabkin <deryabkinda@mail.ru>
2017-11-21 10:49:23 +0700

    Карта PIN начата составляться.

--- user_code/mk_hardware/rk_pins.cpp
+++ user_code/mk_hardware/rk_pins.cpp
@@ -1,1 +1,2 @@
 #include "rk_pins.h"
+
--- user_code/mk_hardware/rk_pins.h
+++ user_code/mk_hardware/rk_pins.h
@@ -2,3 +2,8 @@
 
 #include "pin.h"
 
+//const pin_cfg i2c_sck_pin_cfg               = { GPIOB, { GPIO_PIN_10, GPIO_MODE_AF_PP, GPIO_NOPULL, GPIO_SPEED_FREQ_MEDIUM, GPIO_AF7_USART3 } };
+
+const pin_cfg rk_global_port_pin_cfg[] = {
+
+};
